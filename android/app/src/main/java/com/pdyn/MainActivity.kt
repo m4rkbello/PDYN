9c89d601ae0fa5a4497e732a00a953e3048a826e
m4rkbello <markbello10041999@gmail.com>
2025-07-20 03:10:04 +0800

    ohahay

--- android/app/src/main/java/com/pdyn/MainActivity.kt
+++ android/app/src/main/java/com/pdyn/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.pdyn
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView
 
 class MainActivity : ReactActivity() {
 
@@ -18,5 +20,13 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+      object : DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled) {
+        override fun createRootView(): RNGestureHandlerEnabledRootView {
+          return RNGestureHandlerEnabledRootView(this@MainActivity)
+        }
+      }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null) // This is important for react-native-screens
+  }
 }
--- android/app/src/main/java/com/pdyn/MainApplication.kt
+++ android/app/src/main/java/com/pdyn/MainApplication.kt
@@ -9,6 +9,7 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.swmansion.reanimated.ReanimatedPackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -18,6 +19,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+              add(ReanimatedPackage()) // Add this line for Reanimated
             }
 
         override fun getJSMainModuleName(): String = "index"
