958dcfa8d4656ce6a5a0c2b3612a9892b628026f
m4rkbello <markbello10041999@gmail.com>
2025-07-20 12:52:42 +0800

    OHAHAY

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
-// App.tsx or App.js
-import 'react-native-reanimated'; // 👈 MUST BE FIRST
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigations/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
-import AppNavigator from './src/navigations/AppNavigator'; // adjust path if needed
 
 export default function App() {
   return (
