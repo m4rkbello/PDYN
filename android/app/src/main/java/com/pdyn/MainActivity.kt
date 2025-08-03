73219a42ff7cd10985012c52b9d09c09500834ab
m4rkbello <markbello10041999@gmail.com>
2025-08-03 19:12:05 +0800

    HAD BUG - DEPRECIATED AMPT! HAYS

--- android/app/src/main/java/com/pdyn/MainActivity.kt
+++ android/app/src/main/java/com/pdyn/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.pdyn
 
-import android.os.Build
-import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.ReactRootView
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView // ✅ Make sure this is here!
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView
 
 class MainActivity : ReactActivity() {
     override fun getMainComponentName(): String = "PDYN"
@@ -13,22 +11,8 @@ class MainActivity : ReactActivity() {
     override fun createReactActivityDelegate(): ReactActivityDelegate {
         return object : ReactActivityDelegate(this, mainComponentName) {
             override fun createRootView(): ReactRootView {
-                return RNGestureHandlerEnabledRootView(this@MainActivity) // ✅ Use the correct class
+                return RNGestureHandlerEnabledRootView(this@MainActivity)
             }
         }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            // Preventing memory leaks on Android 12+
-            try {
-                Class.forName("android.app.ActivityThread")
-                    .getDeclaredMethod("currentActivityThread")
-                    .invoke(null)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-        super.onCreate(null)
     }
 }
